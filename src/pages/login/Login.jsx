6bb9b0a0b6c53446992f2a2a824fa3a4fe3ea110
JaeHyeon_Lee <project0858@gmail.com>
2024-11-21 14:28:10 +0900

    [feat] 로그인: slice(login, logout) 함수 연결 중

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import LinkBtn from '../../component/common/LinkBtn'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        
+        console.log(e.target);
+
+        
+      }}>
+        <input type="text" name="username" id="username" required value={"123"}/>
+        <input type="password" name="password" id="password" /* required */  value={"456"}/>
+        <button type="submit">로그인</button>
+      </form>
+    </div>
   )
+}
+function login() {
+
 }
--- src/store/slice/sampleSlice.js
+++ src/store/slice/sampleSlice.js
@@ -1,21 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  value: 0,
-}
+/* 
+[
+  {
+    name1 : ['영화아이디1', '영화아이디2'],
+    isLogin : true
+  },
+  {
+    name2 : ['영화아이디3', '영화아이디4'],
+    isLogin : false
+  }
+]
+*/
+const initialState = {};
 
 const sampleSlice = createSlice({
   name: 'sample',
   initialState,
   reducers: {
-    test: (state, action) => {
-      // state.value = 100
-      state.value += action.payload
+    login: (state, action) => {
+      const { username } = action.payload.username;
+      console.log(username);
+
+      const userInfo = { [`${username}`]: [], inLogin: true };
+      if (!state.some((user) => username in user))
+        return {
+          ...state,
+          userInfo,
+        };
+    },
+    logout: (state, action) => {
+      // username 확인해서 삭제
+      const { username } = action.payload;
+      console.log(username);
     },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { test } = sampleSlice.actions
+export const { test } = sampleSlice.actions;
 
-export default sampleSlice.reducer
+export default sampleSlice.reducer;
